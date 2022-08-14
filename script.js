fb946f0adb3bfbd48a0f780f27d88c0ac2ae79fd
heitorthewizard <heitor.mb27@gmail.com>
2022-08-14 16:10:18 -0300

    improved energy consumption by changing set interval

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ function getCurrentTime() {
     const hours = now.getHours()
     const timeInterval = 6 // since 60 * 6 = 360 ("degrees")
     
+    // crazy calculation to get the positions right on the clock
     secondsHand.style.transform = `rotate(${seconds * timeInterval}deg)`
     minutesHand.style.transform = `rotate(${(minutes * timeInterval) + (seconds / 10)}deg)`
     hoursHand.style.transform = `rotate(${(hours * 30) + (minutes / 2)}deg)`
 
 }
 
-setInterval(getCurrentTime, 100)
+getCurrentTime() // start as soon as the page loads
+
+setInterval(getCurrentTime, 1000) // keep updating time
